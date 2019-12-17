7daa44fb58fda9975f66979b0c744450b43164c9
sesilliayam <sesilliafajar@gmail.com>
2019-12-18 02:54:04 +0700

    fixing jquery

--- resources/views/layouts/resource.blade.php
+++ resources/views/layouts/resource.blade.php
@@ -1,13 +1,13 @@
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Plugin JavaScript -->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('vendor/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
 
 <!-- Custom scripts for this template -->
-{{-- <script src="{{asset('/js/creative.js')}}"></script> --}}
+<script src="{{asset('/js/creative.js')}}"></script> --}}
 <script src="{{asset('/js/creative.min.js')}}"></script>
 <script src="{{asset('/js/myjavascript.js')}}"></script>
 <script src="public/js/bootstrap.bundle.min.js"></script>
