86d37ee6dc9121229e7d6923ea7363566c7b2150
sesilliayam <sesilliafajar@gmail.com>
2019-12-18 03:15:12 +0700

    fixing jquery

--- resources/views/layouts/resource.blade.php
+++ resources/views/layouts/resource.blade.php
@@ -3,7 +3,7 @@
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Plugin JavaScript -->
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+{{-- <script src="vendor/jquery-easing/jquery.easing.min.js"></script> --}}
 {{-- <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script> --}}
 
 <!-- Custom scripts for this template -->
