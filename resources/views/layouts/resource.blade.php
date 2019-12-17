2dc22144bc6cccf476af2a2ae0c08fab8bdc63a6
sesilliayam <sesilliafajar@gmail.com>
2019-12-18 03:17:42 +0700

    fixing jquery

--- resources/views/layouts/resource.blade.php
+++ resources/views/layouts/resource.blade.php
@@ -13,6 +13,6 @@
 <script src="public/js/bootstrap.bundle.min.js"></script>
 
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+{{-- <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> --}}
 @stack('js')
 <!------ Include the above in your HEAD tag ---------->
