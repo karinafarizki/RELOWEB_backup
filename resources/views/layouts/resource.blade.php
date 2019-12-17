758eaa8c5d5ded87584b2f832e3accce43760ef8
sesilliayam <sesilliafajar@gmail.com>
2019-12-18 03:10:07 +0700

    fixing jquery

--- resources/views/layouts/resource.blade.php
+++ resources/views/layouts/resource.blade.php
@@ -2,9 +2,9 @@
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<!-- Plugin JavaScript -->
+{{-- <!-- Plugin JavaScript -->
 <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script> --}}
 
 <!-- Custom scripts for this template -->
 <script src="{{asset('/js/creative.js')}}"></script> --}}
