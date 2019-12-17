c4e76ffd4d58b046eecfad18ebf99a476fd22e0a
sesilliayam <sesilliafajar@gmail.com>
2019-12-18 03:44:50 +0700

    fixing jquery

--- resources/views/layouts/resource.blade.php
+++ resources/views/layouts/resource.blade.php
@@ -1,16 +1,16 @@
 @section('resources')
     <!-- Bootstrap core JavaScript -->
-    <script src="{{asset('/js/jquery.min.js')}}"></script>
-    <script src="{{asset('/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="{{asset('/js/jquery.easing.min.js')}}"></script>
-    <script src="{{asset('/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="/js/jquery.easing.min.js"></script>
+    <script src="/js/jquery.magnific-popup.min.js"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="{{asset('/js/creative.js')}}"></script> --}}
-    <script src="{{asset('/js/creative.min.js')}}"></script>
-    <script src="{{asset('/js/myjavascript.js')}}"></script>
+    <script src="/js/creative.js"></script> --}}
+    <script src="/js/creative.min.js"></script>
+    <script src="/js/myjavascript.js"></script>
 {{-- 
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> --}}
