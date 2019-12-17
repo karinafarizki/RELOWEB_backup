361f84583ad813fc369c35e9f75cdf9450f29129
sesilliayam <sesilliafajar@gmail.com>
2019-12-18 03:40:05 +0700

    fixing jquery

--- resources/views/layouts/resource.blade.php
+++ resources/views/layouts/resource.blade.php
@@ -11,9 +11,9 @@
     <script src="{{asset('/js/creative.js')}}"></script> --}}
     <script src="{{asset('/js/creative.min.js')}}"></script>
     <script src="{{asset('/js/myjavascript.js')}}"></script>
-
+{{-- 
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> --}}
     @stack('js')
     <!------ Include the above in your HEAD tag ---------->
 @endsection
